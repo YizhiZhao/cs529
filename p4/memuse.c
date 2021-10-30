84666204fc480a4f767f66029216a78be8a807de
Yizhi Zhao <eze.zhao@gmail.com>
2021-10-29 21:26:35 -0400

    update p4

--- p4/memuse.c
+++ p4/memuse.c
@@ -3,12 +3,52 @@
 #include <linux/sched.h>
 #include <linux/pid.h>
 
+#define KERNEL_STACK_PAGES 2
+
+// this function calculate the physical memory size with virtual memory begin and end as input
+long cal_physical_memory(struct mm_struct *s_mm, unsigned long start, unsigned long end)
+{
+        unsigned long p_page;
+        long result = 0;
+        pgd_t *pgd;
+        p4d_t *p4d;
+        pud_t *pud;
+        pmd_t *pmd;
+        pte_t *pte;
+
+        // looks down the page table level by level
+        // if the page has an valid entry in page table, the page in currently in physical memory
+        for (p_page = start; p_page < end; p_page += PAGE_SIZE)
+        {
+                pgd = pgd_offset(s_mm, p_page);
+                if (pgd_none(*pgd) || pgd_bad(*pgd))
+                        continue;
+                p4d = p4d_offset(pgd, p_page);
+                if (p4d_none(*p4d) || p4d_bad(*p4d))
+                        continue;
+                pud = pud_offset(p4d, p_page);
+                if (pud_none(*pud) || pud_bad(*pud))
+                        continue;
+                pmd = pmd_offset(pud, p_page);
+                if (pmd_none(*pmd) || pmd_bad(*pmd))
+                        continue;
+                if (!(pte = pte_offset_map(pmd, p_page)))
+                        continue;
+                if (NULL == pte)
+                        continue;
+                result += PAGE_SIZE;
+        }
+
+        return result;
+}
+
 SYSCALL_DEFINE2(memuse_syscall, int, p_id, int, mem_type)
 {
         struct pid *s_pid;
         struct task_struct *s_task;
         struct mm_struct *s_mm;
         struct vm_area_struct *s_vma;
+        unsigned long kernel_stack_start;
 
         printk(KERN_ALERT "memuse_syscall: p_id %d, mem_type %d \n", p_id, mem_type);
 
@@ -43,23 +83,27 @@ SYSCALL_DEFINE2(memuse_syscall, int, p_id, int, mem_type)
                 printk(KERN_ALERT "memuse_syscall: mm is NULL, this is a kernel thread with p_id %d \n", p_id);
                 return -1;
         }
-        // start_stack is the start of stack memory
         if (mem_type == 1) {
-                // mm_struct does not provide the end of stack
-                // so we go through mmap to find the area where stack is and get the end of area
+                // mm_struct provides the start of stack
+                // so we go through mmap to find the area where stack is and get the size of this area
                 for (s_vma = s_mm->mmap; s_vma; s_vma = s_vma->vm_next)
                 {
                         if (s_vma->vm_start <= s_mm->start_stack && s_mm->start_stack <= s_vma->vm_end) {
-                                return s_vma->vm_end - s_mm->start_stack;
+                                return cal_physical_memory(s_mm, s_vma->vm_start, s_vma->vm_end);
                         }
                 }
                 printk(KERN_ALERT "memsue_syscall: unable to find start_stack in mmap\n");
                 return -1;
         }
-        // size of kernel stack is fixed, in x86_64 it is 8kb (2 pages); each thread has its own kernel stack
+         // size of kernel stack is fixed, in x86_64 it is 8kb (2 pages)
         if (mem_type == 2) {
-                return (long) atomic_read(&s_mm->mm_users) * 8192;
+                // task_struct provides the start of kernel stack
+                kernel_stack_start = (unsigned long)(s_task->stack);
+                return cal_physical_memory(s_mm, kernel_stack_start, kernel_stack_start + PAGE_SIZE * KERNEL_STACK_PAGES);
         }
         // start_brk is the start of heap memory, brk is the end of heap memory
-        return s_mm->brk - s_mm->start_brk;
+        else {
+                return cal_physical_memory(s_mm, s_mm->start_brk, s_mm->brk);
+        }
+
 }
